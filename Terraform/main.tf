714b59232e5b0359a64b25d3679c92c6c1578722
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2024-03-18 13:47:28 +0000

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -9,11 +9,6 @@ terraform {
 
 provider "azurerm" {
  features {}
- # These environment variables must be set in your CI/CD pipeline or your local environment
- client_id       = "${env("ARM_CLIENT_ID")}"
- client_secret   = "${env("ARM_CLIENT_SECRET")}" 
- subscription_id = "${env("ARM_SUBSCRIPTION_ID")}" 
- tenant_id       = "${env("ARM_TENANT_ID")}" 
 }
 
 module "lz_vending" {
