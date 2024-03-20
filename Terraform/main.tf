dab6ea07f33768a1e4d3152ab9616dc1843e9888
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2024-03-20 17:27:13 +0000

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -19,39 +19,33 @@ features {}
 
 module "lz_vending" {
   source  = "Azure/lz-vending/azurerm"
-  version = "~>3.4.1" # change this to your desired version, https://www.terraform.io/language/expressions/version-constraints
+  version = "~>3.4.1" # Specify the version as per your requirements
   disable_telemetry = true
 
-  location = var.primary_location
-  subscription_id = var.subscription_id
-
-  network_watcher_resource_group_enabled = false
-
-  # management group association variables
+  location                                       = var.primary_location
+  subscription_id                                = var.subscription_id
+  network_watcher_resource_group_enabled         = false
   subscription_management_group_association_enabled = true
-  subscription_management_group_id                  = "es-landing-zones"
+  subscription_management_group_id               = "es-landing-zones"
+  virtual_network_enabled                        = true
 
-  # virtual network variables
-  virtual_network_enabled = true
   virtual_networks = {
     vnet1 = {
-      name                    = var.virtual_networks_name
-      address_space           = var.address_space
-      resource_group_name     = var.resource_group_name
-      hub_peering_enabled     = var.enable_vnet_peering
-      hub_network_resource_id = "/subscriptions/1ba2c686-9231-4bfd-bb2e-aadb1309cfac/resourceGroups/slz-connectivity-westeurope/providers/Microsoft.Network/virtualNetworks/slz-hub-westeurope"
+      name                          = var.virtual_networks_name
+      address_space                 = var.address_space
+      resource_group_name           = var.resource_group_name
+      hub_peering_enabled           = var.enable_vnet_peering
+      hub_network_resource_id       = "/subscriptions/1ba2c686-9231-4bfd-bb2e-aadb1309cfac/resourceGroups/slz-connectivity-westeurope/providers/Microsoft.Network/virtualNetworks/slz-hub-westeurope"
       hub_peering_use_remote_gateways = false
-
-
     }
+  }
 
   resource_group_creation_enabled = true
+  
   resource_groups = {
     myrg = {
       name     = "My_LZ_Rg"
       location = "westeurope"
     }
   }
-  }
 }
-
