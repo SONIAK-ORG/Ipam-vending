5ae83ffe5a721cea64707ef548c90330d86733d8
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2024-03-20 17:41:44 +0000

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "azurerm" {
  
-#subscription_id = "6ab5ad42-6a7d-4f21-a926-9ce61b67df96"
+
 client_id       = "9ffdadd5-6a69-418a-b754-4689fd9afa7a"
 tenant_id       = "832b0908-3585-4294-a01c-7763fc195006"
 skip_provider_registration = true
