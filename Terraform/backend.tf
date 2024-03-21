91900c697a25a646f1e1c656139f98d23b2ef957
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2024-03-21 14:11:37 +0000

    Update backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -4,7 +4,7 @@ terraform {
     storage_account_name  = "lamdingzonesa"
     container_name        = "tfstate"
     key                   = "terraform.tfstate"
-    use_oidc              = true
+    #use_oidc              = true
     subscription_id = "6ab5ad42-6a7d-4f21-a926-9ce61b67df96"
     tenant_id       = "832b0908-3585-4294-a01c-7763fc195006"
   }
