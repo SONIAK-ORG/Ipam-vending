f8d668955b88cbcb0b895abdc75345228b6de12f
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2024-03-21 16:15:58 +0000

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -32,7 +32,7 @@ module "lz_vending" {
   virtual_networks = {
     vnet1 = {
       name                          = var.virtual_networks_name
-      address_space                 = var.address_space
+      address_space                 = var.address_prefix
       resource_group_name           = var.resource_group_name
       hub_peering_enabled           = var.enable_vnet_peering
       hub_network_resource_id       = "/subscriptions/1ba2c686-9231-4bfd-bb2e-aadb1309cfac/resourceGroups/slz-connectivity-westeurope/providers/Microsoft.Network/virtualNetworks/slz-hub-westeurope"
