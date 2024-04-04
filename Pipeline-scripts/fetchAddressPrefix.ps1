e17b5609024f6feadbbd491826d13f3c0c35f235
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2024-04-05 00:05:17 +0100

    Update fetchAddressPrefix.ps1

--- Pipeline-scripts/fetchAddressPrefix.ps1
+++ Pipeline-scripts/fetchAddressPrefix.ps1
@@ -1,36 +1,36 @@
-# param (
-#   [Parameter()]
-#   [Int]$IPAM_SIZE = "$($Env:IPAM_SIZE)",
-
-#   [Parameter()]
-#   [String]$engineClientId = "$($Env:ENGINE_CLIENT_ID)",
-
-#   [Parameter()]
-#   [String]$appName = "$($Env:APP_NAME)",
-
-#   [Parameter()]
-#   [String]$space = "$($Env:SPACE)",
-
-#   [Parameter()]
-#   [String]$block = "$($Env:BLOCK)"
-# )
-
 param (
   [Parameter()]
-  [Int]$IPAM_SIZE = "24",
+  [Int]$IPAM_SIZE = "$($Env:IPAM_SIZE)",
 
   [Parameter()]
-  [String]$engineClientId = "03885f20-6165-4a52-8eed-45d86b8c9efe",
+  [String]$engineClientId = "$($Env:ENGINE_CLIENT_ID)",
 
   [Parameter()]
-  [String]$appName = "ipam-oxyskfjzsz5pw",
+  [String]$appName = "$($Env:APP_NAME)",
 
   [Parameter()]
-  [String]$space = "Postman",
+  [String]$space = "$($Env:SPACE)",
 
   [Parameter()]
-  [String]$block = "Posted"
+  [String]$block = "$($Env:BLOCK)"
 )
+
+# param (
+#   [Parameter()]
+#   [Int]$IPAM_SIZE = "24",
+
+#   [Parameter()]
+#   [String]$engineClientId = "03885f20-6165-4a52-8eed-45d86b8c9efe",
+
+#   [Parameter()]
+#   [String]$appName = "ipam-oxyskfjzsz5pw",
+
+#   [Parameter()]
+#   [String]$space = "Postman",
+
+#   [Parameter()]
+#   [String]$block = "Posted"
+# )
 
 # Retrieve the access token
 $accessTokenRaw = Get-AzAccessToken -ResourceUrl api://$engineClientId
@@ -40,9 +40,6 @@ $accessTokenSecure = ConvertTo-SecureString $accessToken -AsPlainText
 # Build the request URL
 $requestUrl = "https://$appName.azurewebsites.net/api/spaces/$space/blocks/$block/reservations"
 
-# Printing the values to the console
-Write-Host "Access Token: $accessToken"  # Caution: Sensitive Information
-Write-Host "Request URL: $requestUrl"
 
 $body = @{
     'size' = $IPAM_SIZE
